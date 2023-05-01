b74f17c0b481baf74014401875954da4152e98e2
rohansapra <53691607+rohansapra@users.noreply.github.com>
2023-05-01 17:21:09 +0530

    Update ManageIoTDevicesService.cs

--- AzureIotDeviceOperations/Services/ManageIoTDevicesService.cs
+++ AzureIotDeviceOperations/Services/ManageIoTDevicesService.cs
@@ -11,7 +11,7 @@ namespace AzureIotDeviceOperations.Services
         private static DeviceClient? deviceClient;
         private static RegistryManager? registryManager;
         private static string iotHubUri = "MyLTIIoTHub1.azure-devices.net";
-        private static string iotHubConnectionString = "HostName=MyLTIIoTHub1.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=6D2Pr6zGlotReChLNtqCYj5z/juLcKykArVvt+ra6dE=";
+        private static string iotHubConnectionString = "HostName=MyLTIIoTHub1.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=5yxURy4nmgZ1JBqfbxD0jnZ6sje7cKkqA5hMTopfLSg=";
 
         public static DeviceClient AddDevice(string deviceName, bool isIoTEdge)
         {
